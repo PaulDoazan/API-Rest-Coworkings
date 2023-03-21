1f399f74ab83471ddcca33a04f7cdfec85e348d7
pdoazan <paul.doazan@gmail.com>
2023-03-21 10:19:52 +0100

    Add first route

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
-const port = 1234
+const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World !')
 })
+
+app.get('/api/coworkings/:id', (req, res) => {
+    const coworkingId = req.params.id;
+    // Utiliser coworkingId pour récupérer les données correspondantes dans la base de données
+    res.send(`Vous avez demandé les informations pour le coworking numéro ${coworkingId}`);
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
