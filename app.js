ab1fde02e2637632f1bd1fd22954d79841f1fa33
pdoazan <paul.doazan@gmail.com>
2023-03-24 16:13:52 +0100

    Add 404 error handler

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ require('./src/routes/createCoworking')(app)
 require('./src/routes/updateCoworking')(app)
 require('./src/routes/deleteCoworking')(app)
 
+app.use((req, res) => {
+    const message = 'Impossible de trouver la ressource demandée ! Essayez une autre url.'
+    res.status(404).json({ message })
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
@@ -27,38 +32,3 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
     res.send('Hello World !')
 })
-/*
-app.post('/api/coworkings', (req, res) => {
-    const id = getUniqueId(mockCoworkings)
-    // petit rappel sur le destructuring
-    const coworkingCreated = { ...req.body, ...{ id: id, created: new Date() } }
-    mockCoworkings.push(coworkingCreated)
-    const message = `Le coworking ${coworkingCreated.name} a bien été créé.`
-    res.json(success(message, coworkingCreated))
-})
-
-app.put('/api/coworkings/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const coworkingUpdated = { ...req.body, id: id }
-
-    mockCoworkings = mockCoworkings.map(coworking => {
-        return coworking.id === id ? coworkingUpdated : coworking;
-    })
-
-    const msg = `Le coworking ${coworkingUpdated.name} a bien été modifié.`
-    res.json(success(msg, coworkingUpdated))
-})
-
-app.delete('/api/coworkings/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    const coworkingDeleted = mockCoworkings.find(coworking => coworking.id === id)
-
-    mockCoworkings.forEach(coworking => {
-        if (coworking.id !== id) return coworking
-    });
-
-    const msg = `Le coworking ${coworkingDeleted.name} a bien été supprimé.`
-
-    res.json(success(msg, mockCoworkings))
-})
-*/
